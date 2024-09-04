752fd73d937b076d007a8697ee521cf5f2b46249
Antonio Jimeno Yepes <antonio@unstructured.io>
2024-09-03 19:15:45 -0700

    Setup for GPU/CPU installation

--- setup.py
+++ setup.py
@@ -1,31 +1,66 @@
 from distutils.core import setup, Extension
 from setuptools.command.build_ext import build_ext
+import subprocess
 import os
 
-os.environ['CFLAGS'] = '-O3 -gstabs -march=native'
-os.environ['LDFLAGS'] = '-O3 -gstabs -march=native'
 
-module1 = Extension('SimulatorGPU',
-                    sources = ['wrapper.cu'],
-                    extra_compile_args = ["-O3", "-arch=compute_86", "-code=sm_86", "-Xptxas=-O3", "--compiler-options", "-fPIC", "--compiler-options", "-march=native"])
+try:
+    subprocess.check_output("nvidia-smi")
+    os.environ["CFLAGS"] = "-O3 -gstabs -march=native"
+    os.environ["LDFLAGS"] = "-O3 -gstabs -march=native"
+    module1 = Extension(
+        "SimulatorGPU",
+        sources=["wrapper.cu"],
+        extra_compile_args=[
+            "-O3",
+            "-arch=compute_86",
+            "-code=sm_86",
+            "-Xptxas=-O3",
+            "--compiler-options",
+            "-fPIC",
+            "--compiler-options",
+            "-march=native",
+        ],
+    )
 
-class CUDA_build_ext(build_ext):
-    """
-    Custom build_ext command that compiles CUDA files.
-    Note that all extension source files will be processed with this compiler.
-    """
-    def build_extensions(self):
-        self.compiler.src_extensions.append('.cu')
-        self.compiler.set_executable('compiler_so', 'nvcc')
-        self.compiler.set_executable('linker_so', 'nvcc --shared')
-        if hasattr(self.compiler, '_c_extensions'):
-            self.compiler._c_extensions.append('.cu')  # needed for Windows
-        self.compiler.spawn = self.spawn
-        build_ext.build_extensions(self)
+    class CUDA_build_ext(build_ext):
+        """
+        Custom build_ext command that compiles CUDA files.
+        Note that all extension source files will be processed with this compiler.
+        """
 
+        def build_extensions(self):
+            self.compiler.src_extensions.append(".cu")
+            self.compiler.set_executable("compiler_so", "nvcc")
+            self.compiler.set_executable("linker_so", "nvcc --shared")
+            if hasattr(self.compiler, "_c_extensions"):
+                self.compiler._c_extensions.append(".cu")  # needed for Windows
+            self.compiler.spawn = self.spawn
+            build_ext.build_extensions(self)
 
-setup (name = 'SimulatorGPU',
-       version = '1.0',
-       description = 'GPU version of the ARC simulator',
-       ext_modules = [module1],
-      cmdclass={'build_ext': CUDA_build_ext})
+    print("SimulatorGPU is being built!")
+    setup(
+        name="SimulatorGPU",
+        version="1.0",
+        description="GPU version of the ARC simulator",
+        ext_modules=[module1],
+        cmdclass={"build_ext": CUDA_build_ext},
+    )
+except Exception:
+    print("No GPU version available")
+
+print("SimulatorCPU is being built!")
+os.environ["CFLAGS"] = "-O3 -march=native"
+os.environ["LDFLAGS"] = "-O3 -march=native"
+
+moduleCPU = Extension(
+        "SimulatorCPU",
+        sources=["wrapper.cpp"],
+        extra_compile_args=["-g", "-O3", "-march=native", "-std=c++11", "-DSETUP_BUILDING_CPU", "-Wsign-compare"],
+)
+setup(
+        name="SimulatorCPU",
+        version="1.0",
+        description="Fast version of the ARC simulator",
+        ext_modules=[moduleCPU],
+)
